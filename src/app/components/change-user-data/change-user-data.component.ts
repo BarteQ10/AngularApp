d422256a4ffb834cb46229413881e2091534d701
Bartek <barteq2345@gmail.com>
2024-04-27 00:05:14 +0200

    Added function change user data

--- src/app/components/change-user-data/change-user-data.component.ts
+++ src/app/components/change-user-data/change-user-data.component.ts
@@ -1,12 +1,77 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { MessageService } from 'primeng/api';
+import { UserService } from '../../services/user.service';
+import { User } from '../../interfaces/auth';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-change-user-data',
   standalone: true,
-  imports: [],
+  imports: [CardModule, InputTextModule, ReactiveFormsModule, ButtonModule, RouterLink, CommonModule, HttpClientModule],
   templateUrl: './change-user-data.component.html',
   styleUrl: './change-user-data.component.css'
 })
-export class ChangeUserDataComponent {
 
+export class ChangeUserDataComponent implements OnInit {
+  disabled = true;
+  userDataForm: FormGroup;
+  constructor(
+    private fb: FormBuilder,
+    private userService: UserService,
+    private messageService: MessageService,
+    private router: Router
+  ) {
+    this.userDataForm = this.fb.group({
+      FirstName: [null, Validators.required],
+      Password: [null, Validators.required],
+      LastName: [null, Validators.required],
+      BirthDate: [null, Validators.required],
+      Sex: [null], // Handle nullable types appropriately based on your logic
+      DefaultAddress: [null, Validators.required],
+      DefaultPostalcode: [null, Validators.required],
+      DefaultLocation: [null, Validators.required],
+      Phone: [null, Validators.required],
+      Mail: [null],
+      Active: [null],
+      Country: [null]
+    });
+  }
+
+  ngOnInit() {
+    // Pobierz aktualne dane użytkownika i wypełnij nimi formularz
+    this.userService.getUserData().subscribe(
+      (userData: Partial<User>) => {
+        this.userDataForm.patchValue(userData);
+      },
+      (error) => {
+        console.error(error);
+        this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Failed to fetch user data' });
+      }
+    );
+  }
+
+  submitDetails() {
+    if (this.userDataForm.valid) {
+      const userData = this.userDataForm.value;
+
+      this.userService.updateUserData(userData).subscribe(
+        () => {
+          this.messageService.add({ severity: 'success', summary: 'Success', detail: 'User data updated successfully' });
+          // Opcjonalnie przekieruj gdziekolwiek po aktualizacji danych użytkownika
+        },
+        (error) => {
+          console.error(error);
+          this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Failed to update user data' });
+        }
+      );
+    } else {
+      // Obsłuż formularz nieprawidłowy
+    }
+  }
 }
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,39 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../interfaces/auth';
+import { AuthService } from './auth.service';
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private baseUrl = 'http://127.0.0.1:8000'; // Bazowy adres API
+
+  constructor(private http: HttpClient, private authService: AuthService) { }
+
+  // Pobierz dane użytkownika z serwera
+  getUserData(): Observable<User> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${this.getAuthToken()}` // Dodaj token autoryzacyjny
+    });
+    const userId = this.authService.getUserId(this.getAuthToken())
+    return this.http.get<User>(`${this.baseUrl}/get-user-by-id/${userId}`, { headers });
+  }
+
+  // Aktualizuj dane użytkownika na serwerze
+  updateUserData(userData: User): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${this.getAuthToken()}` // Dodaj token autoryzacyjny
+    });
+    const userId = this.authService.getUserId(this.getAuthToken())
+
+    return this.http.put(`${this.baseUrl}/edit-user/${userId}/`, userData, { headers });
+  }
+
+  // Pobierz token autoryzacyjny z sesji
+  private getAuthToken(): string {
+    return sessionStorage.getItem('jwt_token') || ''; // Sprawdź, czy token istnieje w sesji
+  }
+}
